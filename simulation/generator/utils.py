bbeb1af670439ed9aff68cc352a21cae3f8cf663
Nick Belzer <me@nickbelzer.me>
2021-09-28 23:00:21 +0200

    Add missing functions to simulation.generator.uitls
    Due to an oversight in the cleaning of the project there was one
    class and function missing.

--- simulation/generator/utils.py
+++ simulation/generator/utils.py
@@ -1,3 +1,5 @@
+from dataclasses import dataclass
+from typing import Optional
 import re
 import csv
 
@@ -15,3 +17,42 @@ def read_resource_map(from_file) -> dict[str, int]:
             if size > 0:
                 resource_map[identifier] = size
     return resource_map
+
+@dataclass
+class Page:
+    """Represents a web page."""
+    url: str
+    outgoing_links: set[str]
+    resources: set[str]
+
+def read_page_map(path) -> dict[str, Page]:
+    CLEAN_URL_REGEX = re.compile("^https?://([^?]*)", re.IGNORECASE)
+    def clean_url(url: str) -> Optional[str]:
+        """Removes the prefix 'https?://' and any parameters from the url.
+
+        Also filters out any url that doesn't conform to the regex by
+        returning `None`.
+        """
+        match = CLEAN_URL_REGEX.match(url)
+        if match == None:
+            return None
+        return match.group(1)
+
+    def clean_urls(urls: list[str]) -> list[str]:
+        return list(filter(lambda x: x != None and len(x) > 0, map(clean_url, urls)))
+
+    """Read a page map from file."""
+    page_map = {}
+    with open(path, 'r') as content_map:
+        for line in content_map:
+            parsed = line.split("\n")[0].split(';')
+            if len(parsed) != 3:
+                continue
+            origin = clean_url(parsed[0])
+            if origin == None:
+                continue
+            links = set(clean_urls([ x for x in parsed[1].split(' ') if len(x) > 0 ]))
+            resources = set(clean_urls([ x for x in parsed[2].split(' ') if len(x) > 0 ]))
+            page_map[origin] = Page(url=origin, outgoing_links=links, resources=resources)
+
+    return page_map
